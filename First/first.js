855d71a3977b00e96fe97dbaaccbe1310084bbb9
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-09-10 22:16:28 +0530

    learning nodejs

---
+++ First/first.js
@@ -0,0 +1,37 @@
+// function sum(){
+//     console.log("sum:",3+4);
+// }
+// module.exports = sum
+
+// -----------------------------------------------------------------------------
+
+
+// function sum(){
+//     console.log("sum:",3+4);
+// }
+// function sub(){
+//     console.log("sub:",7-4);
+// }
+// module.exports = {sum,sub}
+
+// -----------------------------------------------------------------------------
+
+
+// function sum(){
+//     console.log("sum:",3+4);
+// }
+// function sub(){
+//     console.log("sub:",7-4);
+// }
+// module.exports = {f1:sum,f2:sub}
+
+// -----------------------------------------------------------------------------
+
+
+function sum(){
+    console.log("sum:",3+4);
+}
+function sub(){
+    console.log("sub:",7-4);
+}
+module.exports = {sum,sub}
---
+++ First/main.js
@@ -0,0 +1,27 @@
+// const sum = require("./first")
+// console.log("Main File")
+// sum()
+
+// -----------------------------------------------------------------------------
+
+
+// const fun = require("./first")
+// console.log("Main File")
+// fun.sum()
+// fun.sub()
+
+// -----------------------------------------------------------------------------
+
+// const fun = require("./first")
+// console.log("Main File")
+
+// fun.f1()
+// fun.f2() 
+
+// -----------------------------------------------------------------------------
+
+let{sum,sub} = require("./first")
+console.log("Main File")
+
+sum()
+sub() 
